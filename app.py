20773477ef8528ba87f7f71bcff2e16e99c2fdd4
DESKTOP-DFD03FF\임시 <seungs0503@gmail.com>
2023-10-09 01:11:06 -0700

    bug fix

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from *flask* import *Flask*
+from flask import Flask
 
-*app* = *Flask*(*__name__*)
+app = Flask(__name__)
 
-@app.*route*("/")
+@app.route("/")
 def hello_world():
     return "<h1>Hello World!</h1>"
 
